7d866fb6adc2ed2d9c7e125d75b307021caf9017
GrantHS <6nohx1@gmail.com>
2022-10-19 21:15:37 -0700

    idek

--- Emit-Split/Assets/Scripts/Lighbot/LightMovement.cs
+++ Emit-Split/Assets/Scripts/Lighbot/LightMovement.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LightMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject spotlight;
+    public float speed = 10f;
+    Vector2 botVector;
+    private bool spotlightOn;
+
+    private void Update()
     {
-        
+        transform.Translate(botVector.x * speed * Time.deltaTime, 0, botVector.y * speed * Time.deltaTime);
+
+        if (spotlightOn)
+        {
+            spotlight.SetActive(true);
+        }
+        else
+        {
+            spotlight.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Movement(InputAction.CallbackContext context)
     {
-        
+
+
+        Debug.Log(context);
+        botVector = context.ReadValue<Vector2>();
+
+    }
+    public void SpotOn(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
+
+        if (context.performed)
+        {
+            if (spotlightOn)
+            {
+                spotlightOn = false;
+            }
+            else
+            {
+                spotlightOn = true;
+            }
+        }
     }
 }
