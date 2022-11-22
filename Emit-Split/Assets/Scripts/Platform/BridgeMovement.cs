6e2c0c5a9e813ae6b0886387a86dbafede5e05b3
GrantHS <6nohx1@gmail.com>
2022-11-22 13:26:47 -0800

    Revert "Level 3 Added"
    This reverts commit d4cafc235ebdb27acf4003e1e6543cd2d62b3795.

--- Emit-Split/Assets/Scripts/Platform/BridgeMovement.cs
+++ Emit-Split/Assets/Scripts/Platform/BridgeMovement.cs
@@ -2,24 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Adding this to a platform makes the platform move vertically between assigned bounds
+//Adding this to a platform makes the platform move vertically AND rotate on the y axis
 public class BridgeMovement : MonoBehaviour
 {
     //the speed that the platform moves
-    private float speed =5f;
+    private float speed = 5f;
+    private float spinSpeed = 30f;
+
+    //the vertical position of the object
+    private Vector3 vertPos;
 
     //determine vertical direction
     public bool goingUp;
 
-    //Points at which the platform will stop and go the other way and their respective position
+    //Points at which the platform will stop and go the other way
+    //[SerializeField]
     public GameObject upperBound;
     private Vector3 upPos;
+    //[SerializeField]
     public GameObject lowerBound;
     private Vector3 downPos;
     
     // Start is called before the first frame update
     void Start()
     {
+        vertPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         upPos = upperBound.transform.position;
         downPos = lowerBound.transform.position;
         goingUp = true;
@@ -30,6 +37,7 @@ public class BridgeMovement : MonoBehaviour
     void Update()
     {
         UpAndDown();
+        //transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
     }
 
     //Method to move the platform up and down
