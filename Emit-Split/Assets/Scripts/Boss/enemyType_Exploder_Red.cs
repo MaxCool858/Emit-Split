da0f9b4e7555c13caf3b0fb1ada31d1caa1d6520
MaxCool858 <99841699+MaxCool858@users.noreply.github.com>
2022-12-08 00:53:17 -0800

    stuff

---
+++ Emit-Split/Assets/Scripts/Boss/enemyType_Exploder_Red.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyType_Exploder_Red : EnemyClass
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 10;
+        self = this.gameObject;
+        enemycontrol = GetComponent<CharacterController>();
+        EnemyTypeNum = 55;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.name);
+        if (other.name == "Fist")
+        {
+            LoseHealth(1);
+        }
+    }
+}
